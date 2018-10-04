beae5c47e4e730c01a9d685a262647185c229f31
nemosminer <slbaker@hotmail.co.nz>
2018-10-04 18:19:16 +1300

    Create ccminerhmq1725.ps1

---
+++ Miners/ccminerhmq1725.ps1
@@ -0,0 +1,24 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+
+$Path = ".\Bin\NVIDIA-Tpruvot23\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminerx16r-x16s/releases/download/v0.5/ccminerx16rx16s32-bit.7z"
+
+$Commands = [PSCustomObject]@{
+    "hmq1725" = " -d $($Config.SelGPUCC) -N 1" #hmq1725 (2080)
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "ccminer"
+        Port = $Variables.NVIDIAMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
+    }
+}
